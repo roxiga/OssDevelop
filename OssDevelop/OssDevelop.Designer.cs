b49c5c50ca055b5c98c47c65adbdfb715f7d41fa
roxiga <takeshi@vixar.jp>
2022-05-19 16:54:19 +0900

    Add files via upload

---
+++ OssDevelop/OssDevelop.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace OssDevelop
+{
+    partial class OssDevelop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        UserControlYear userControlYear = null;
+        UserControlMonth userControlMonth = null;
+        UserControlDiary userControlDiary = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OssDevelop));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.searchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.findNextToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbToday = new System.Windows.Forms.Label();
+            this.buttonDiary = new System.Windows.Forms.Button();
+            this.buttonMonth = new System.Windows.Forms.Button();
+            this.buttonYear = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnToday = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.searchToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(796, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // searchToolStripMenuItem
+            // 
+            this.searchToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.findToolStripMenuItem,
+            this.findNextToolStripMenuItem});
+            this.searchToolStripMenuItem.Name = "searchToolStripMenuItem";
+            this.searchToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.searchToolStripMenuItem.Text = "Search";
+            // 
+            // findToolStripMenuItem
+            // 
+            this.findToolStripMenuItem.Name = "findToolStripMenuItem";
+            this.findToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.F)));
+            this.findToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.findToolStripMenuItem.Text = "Find";
+            this.findToolStripMenuItem.Click += new System.EventHandler(this.findToolStripMenuItem_Click);
+            // 
+            // findNextToolStripMenuItem
+            // 
+            this.findNextToolStripMenuItem.Name = "findNextToolStripMenuItem";
+            this.findNextToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F3;
+            this.findNextToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.findNextToolStripMenuItem.Text = "Find Next";
+            this.findNextToolStripMenuItem.Click += new System.EventHandler(this.findNextToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1;
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(13, 70);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(771, 411);
+            this.panel1.TabIndex = 1;
+            // 
+            // lbToday
+            // 
+            this.lbToday.AutoSize = true;
+            this.lbToday.Font = new System.Drawing.Font("メイリオ", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbToday.Location = new System.Drawing.Point(15, 30);
+            this.lbToday.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbToday.Name = "lbToday";
+            this.lbToday.Size = new System.Drawing.Size(351, 28);
+            this.lbToday.TabIndex = 137;
+            this.lbToday.Text = "Tuesday 12 December [12], 2017";
+            // 
+            // buttonDiary
+            // 
+            this.buttonDiary.Location = new System.Drawing.Point(695, 30);
+            this.buttonDiary.Name = "buttonDiary";
+            this.buttonDiary.Size = new System.Drawing.Size(85, 22);
+            this.buttonDiary.TabIndex = 136;
+            this.buttonDiary.Text = "Diary";
+            this.buttonDiary.UseVisualStyleBackColor = true;
+            this.buttonDiary.Click += new System.EventHandler(this.ButtonDiary_Click);
+            // 
+            // buttonMonth
+            // 
+            this.buttonMonth.Location = new System.Drawing.Point(604, 30);
+            this.buttonMonth.Name = "buttonMonth";
+            this.buttonMonth.Size = new System.Drawing.Size(85, 22);
+            this.buttonMonth.TabIndex = 135;
+            this.buttonMonth.Text = "Month";
+            this.buttonMonth.UseVisualStyleBackColor = true;
+            this.buttonMonth.Click += new System.EventHandler(this.ButtonMonth_Click);
+            // 
+            // buttonYear
+            // 
+            this.buttonYear.Location = new System.Drawing.Point(513, 30);
+            this.buttonYear.Name = "buttonYear";
+            this.buttonYear.Size = new System.Drawing.Size(85, 22);
+            this.buttonYear.TabIndex = 138;
+            this.buttonYear.Text = "Year";
+            this.buttonYear.UseVisualStyleBackColor = true;
+            this.buttonYear.Click += new System.EventHandler(this.buttonYear_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(461, 30);
+            this.btnNext.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(23, 22);
+            this.btnNext.TabIndex = 141;
+            this.btnNext.Text = ">";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnToday
+            // 
+            this.btnToday.Location = new System.Drawing.Point(405, 30);
+            this.btnToday.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnToday.Name = "btnToday";
+            this.btnToday.Size = new System.Drawing.Size(48, 22);
+            this.btnToday.TabIndex = 140;
+            this.btnToday.Text = "&Today";
+            this.btnToday.UseVisualStyleBackColor = true;
+            this.btnToday.Click += new System.EventHandler(this.btnToday_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(374, 30);
+            this.btnPrevious.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(23, 22);
+            this.btnPrevious.TabIndex = 139;
+            this.btnPrevious.Text = "<";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // OssDevelop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 490);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnToday);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.buttonYear);
+            this.Controls.Add(this.lbToday);
+            this.Controls.Add(this.buttonDiary);
+            this.Controls.Add(this.buttonMonth);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.MaximizeBox = false;
+            this.Name = "OssDevelop";
+            this.Text = "OssDevelop";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem searchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem findNextToolStripMenuItem;
+        private Panel panel1;
+        private Label lbToday;
+        private Button buttonDiary;
+        private Button buttonMonth;
+        private Button buttonYear;
+        private Button btnNext;
+        private Button btnToday;
+        private Button btnPrevious;
+    }
+}
+
---
+++ OssDevelop/OssDevelop.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OssDevelop
+{
+    public partial class OssDevelop : Form
+    {
+        private AboutBox? aboutBox = null;
+        private SearchForm? searchForm = null;
+        private int predate = 0;
+        public DateTime theDate;
+
+        public OssDevelop()
+        {
+            userControlYear = new UserControlYear(this);
+            userControlMonth = new UserControlMonth(this);
+            userControlDiary = new UserControlDiary(this);
+            InitializeComponent();
+            panel1.Controls.Add(userControlYear);
+            panel1.Controls.Add(userControlMonth);
+            panel1.Controls.Add(userControlDiary);
+            theDate = DateTime.Today;
+            SetMonth();
+        }
+
+        public void updateLabels(DateTime date)
+        {
+            theDate = date;
+            userControlMonth.theDate = date;
+            userControlDiary.theDate = date;
+            lbToday.Text = theDate.ToString("yyyy年 MMMM d日 dddd");
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (aboutBox == null)
+                aboutBox = new AboutBox();
+            aboutBox.ShowDialog();
+        }
+
+        private void findToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (searchForm == null)
+                searchForm = new SearchForm();
+            if (searchForm.ShowDialog() == DialogResult.OK)
+            {
+                predate = 0;
+                predate = search(predate);
+            }
+        }
+
+        private void findNextToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            predate = search(predate);
+        }
+
+        private int search(int pre)
+        {
+           if (searchForm != null)
+           {
+                var keyword = searchForm.GetText();
+                bool matchCase = searchForm.MatchCase();
+                bool wholeWord = searchForm.WholeWord();
+                bool regularX = searchForm.RegularExpression();
+                DataBase db = new DataBase();
+                Diary diary = db.findText(keyword, matchCase, wholeWord, regularX, pre);
+                if (diary.date > 0)
+                {
+                    int year = (int)(diary.date / 10000);
+                    int month = (int)(diary.date / 100) % 100;
+                    int day = diary.date % 100;
+                    theDate = new DateTime(year, month, day);
+                    SetDiary();
+                    return diary.date;
+                }
+            }
+            return 0;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonYear_Click(object sender, EventArgs e)
+        {
+            userControlYear.Visible = true;
+            userControlMonth.Visible = false;
+            userControlDiary.Visible = false;
+            userControlYear.SetSelectDate(theDate);
+        }
+
+        private void ButtonMonth_Click(object sender, EventArgs e)
+        {
+            SetMonth();
+        }
+
+        public void SetMonth()
+        {
+            userControlYear.Visible = false;
+            userControlMonth.Visible = true;
+            userControlDiary.Visible = false;
+            updateLabels(theDate);
+            userControlMonth.updateCalendar(true);
+        }
+
+        private void ButtonDiary_Click(object sender, EventArgs e)
+        {
+            SetDiary();
+        }
+
+        public void SetDiary()
+        {
+            userControlYear.Visible = false;
+            userControlMonth.Visible = false;
+            userControlDiary.Visible = true;
+            updateLabels(theDate);
+            userControlMonth.updateCalendar(true);
+            userControlDiary.LoadDataBase();
+        }
+
+        private void btnToday_Click(object sender, EventArgs e)
+        {
+            theDate = DateTime.Today;
+            if (userControlYear.Visible)
+            {
+                updateLabels(theDate);
+                userControlMonth.updateCalendar(true);
+                userControlYear.SetSelectDate(DateTime.Today);
+            }
+            else if (userControlMonth.Visible)
+            {
+                updateLabels(theDate);
+                userControlMonth.updateCalendar(true);
+            }
+            else
+            {
+                SetDiary();
+            }
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (userControlYear.Visible)
+            {
+                theDate = theDate.AddYears(-1);
+                userControlMonth.updateTheDate(theDate.Year, theDate.Month, theDate.Day);
+                userControlYear.SetSelectDate(theDate);
+            }
+            else if (userControlMonth.Visible)
+            {
+                theDate = theDate.AddMonths(-1);
+                userControlMonth.updateTheDate(theDate.Year, theDate.Month, theDate.Day);
+            }
+            else
+            {
+                theDate = theDate.AddDays(-1);
+                userControlMonth.updateTheDate(theDate.Year, theDate.Month, theDate.Day);
+                SetDiary();
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (userControlYear.Visible)
+            {
+                theDate = theDate.AddYears(1);
+                userControlMonth.updateTheDate(theDate.Year, theDate.Month, theDate.Day);
+                userControlYear.SetSelectDate(theDate);
+            }
+            else if (userControlMonth.Visible)
+            {
+                theDate = theDate.AddMonths(1);
+                userControlMonth.updateTheDate(theDate.Year, theDate.Month, theDate.Day);
+            }
+            else
+            {
+                theDate = theDate.AddDays(1);
+                userControlMonth.updateTheDate(theDate.Year, theDate.Month, theDate.Day);
+                SetDiary();
+            }
+        }
+    }
+}
+
