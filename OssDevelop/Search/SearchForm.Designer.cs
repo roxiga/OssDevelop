5993372f3386ff98c5707caae66bc6825e270c74
roxiga <takeshi@vixar.jp>
2022-05-19 17:01:17 +0900

    Add files via upload

---
+++ OssDevelop/Search/SearchForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace OssDevelop
+{
+    partial class SearchForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btnFind = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.cbMatchCase = new System.Windows.Forms.CheckBox();
+            this.cbWholeWord = new System.Windows.Forms.CheckBox();
+            this.cbRegular = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Find:";
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(58, 33);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(234, 20);
+            this.textBox.TabIndex = 0;
+            this.textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // btnFind
+            // 
+            this.btnFind.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnFind.Location = new System.Drawing.Point(312, 31);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.Size = new System.Drawing.Size(101, 23);
+            this.btnFind.TabIndex = 3;
+            this.btnFind.Text = "&Find";
+            this.btnFind.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(312, 70);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(101, 23);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Cance&l";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // cbMatchCase
+            // 
+            this.cbMatchCase.AutoSize = true;
+            this.cbMatchCase.Location = new System.Drawing.Point(58, 74);
+            this.cbMatchCase.Name = "cbMatchCase";
+            this.cbMatchCase.Size = new System.Drawing.Size(82, 17);
+            this.cbMatchCase.TabIndex = 1;
+            this.cbMatchCase.Text = "Match &case";
+            this.cbMatchCase.UseVisualStyleBackColor = true;
+            // 
+            // cbWholeWord
+            // 
+            this.cbWholeWord.AutoSize = true;
+            this.cbWholeWord.Location = new System.Drawing.Point(155, 74);
+            this.cbWholeWord.Name = "cbWholeWord";
+            this.cbWholeWord.Size = new System.Drawing.Size(83, 17);
+            this.cbWholeWord.TabIndex = 2;
+            this.cbWholeWord.Text = "&Whole word";
+            this.cbWholeWord.UseVisualStyleBackColor = true;
+            this.cbWholeWord.CheckedChanged += new System.EventHandler(this.cbWholeWord_CheckedChanged);
+            // 
+            // cbRegular
+            // 
+            this.cbRegular.AutoSize = true;
+            this.cbRegular.Location = new System.Drawing.Point(58, 97);
+            this.cbRegular.Name = "cbRegular";
+            this.cbRegular.Size = new System.Drawing.Size(116, 17);
+            this.cbRegular.TabIndex = 5;
+            this.cbRegular.Text = "Regular e&xpression";
+            this.cbRegular.UseVisualStyleBackColor = true;
+            this.cbRegular.CheckedChanged += new System.EventHandler(this.cbRegular_CheckedChanged);
+            // 
+            // SearchForm
+            // 
+            this.AcceptButton = this.btnFind;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(436, 133);
+            this.Controls.Add(this.cbRegular);
+            this.Controls.Add(this.cbWholeWord);
+            this.Controls.Add(this.cbMatchCase);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnFind);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "SearchForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Find";
+            this.Activated += new System.EventHandler(this.SearchForm_Activated);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button btnFind;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.CheckBox cbMatchCase;
+        private System.Windows.Forms.CheckBox cbWholeWord;
+        private System.Windows.Forms.CheckBox cbRegular;
+    }
+}
--- OssDevelop/Search/SearchForm.cs
+++ OssDevelop/Search/SearchForm.cs
@@ -1,1 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
+namespace OssDevelop
+{
+    public partial class SearchForm : Form
+    {
+        public SearchForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Return))
+            {
+                DialogResult = DialogResult.OK;
+            }
+        }
+
+        public string GetText()
+        {
+            return textBox.Text;
+        }
+
+        public bool MatchCase()
+        {
+            return cbMatchCase.Checked;
+        }
+
+        public bool WholeWord()
+        {
+            return cbWholeWord.Checked;
+        }
+
+        public bool RegularExpression()
+        {
+            return cbRegular.Checked;
+        }
+
+        private void SearchForm_Activated(object sender, EventArgs e)
+        {
+            textBox.Focus();
+        }
+
+        private void cbRegular_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbRegular.Checked)
+                cbWholeWord.Checked = false;
+        }
+
+        private void cbWholeWord_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbWholeWord.Checked)
+                cbRegular.Checked = false;
+        }
+    }
+}
