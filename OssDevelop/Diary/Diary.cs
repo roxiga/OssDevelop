8be79bd110082a29e9a78a43ecd62f9c0b7d147d
roxiga <takeshi@vixar.jp>
2022-05-19 16:55:42 +0900

    Add files via upload

--- OssDevelop/Diary/Diary.cs
+++ OssDevelop/Diary/Diary.cs
@@ -1,1 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace OssDevelop
+{
+    public class Diary
+    {
+        public int date = 12340101;
+        public string title = "";
+        public string text = "";
+        public Image? image = null;
+
+        public Diary(int date)
+        {
+            this.date = date;
+        }
+    }
+}
