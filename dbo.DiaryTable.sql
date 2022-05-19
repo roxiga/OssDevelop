280172b357733a7560ba4e8f90de69700ae8dece
roxiga <takeshi@vixar.jp>
2022-05-19 16:51:35 +0900

    Add files via upload

---
+++ dbo.DiaryTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[DiaryTable] (
+    [Id]    INT            IDENTITY (1, 1) NOT NULL,
+    [date]  INT            NULL,
+    [title] NVARCHAR (50)  NULL,
+    [text]  NVARCHAR (MAX) NULL,
+    [image] IMAGE          NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
